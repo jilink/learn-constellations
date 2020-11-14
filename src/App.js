78cfaca27248ce9af941b8a80e57363bd5ce47f4
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-14 12:27:53 +0100

    Display Random constellation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import styles from "./App.module.css";
+import ConstellationSketcher, {
+  constellationNames,
+  categories,
+} from "react-constellation-sketcher";
 
-function App() {
+const API_BASE = "https://www.strudel.org.uk/lookUP/json/?name=";
+const getConstellationUrl = (name) => `${API_BASE}${name}`;
+
+const Constellation = ({ name, img }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{name}</p>
+      <img src={img} />
     </div>
   );
-}
+};
+
+const GameContainer = () => {
+  const [constellation, setConstellation] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  useEffect(() => {
+    getRandomConstellation();
+  }, []);
+  const getRandomConstellation = () => {
+    setIsLoading(true);
+    const index = Math.floor(Math.random() * constellationNames.length);
+
+    axios
+      .get(getConstellationUrl(constellationNames[index]))
+      .then((response) => {
+        setConstellation(response.data);
+        setIsLoading(false);
+      });
+  };
+
+  const handleNextConstellation = () => {
+    getRandomConstellation();
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <p> Loading ...</p>
+      ) : (
+        <Constellation
+          name={constellation.target.name}
+          img={constellation.image.src}
+        />
+      )}
+      <button onClick={handleNextConstellation}>NEXT</button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Learn Constellations</h1>
+      <GameContainer />
+    </div>
+  );
+};
 
 export default App;
