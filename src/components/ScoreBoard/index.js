241c4d61cfde926c60e468844b21727a873f0864
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-11-21 09:40:28 +0100

    some style to scoreboard

--- src/components/ScoreBoard/index.js
+++ src/components/ScoreBoard/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Firebase, { FirebaseContext } from "../Firebase/index";
+import Table from "react-bootstrap/Table";
+import styles from "./style.module.css";
 
-const ScoreBoard = () => {
+const ScoreBoard = ({ limit = 10 }) => {
   const [scoreList, setScoreList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
     const firebase = new Firebase();
-    firebase.getTopScore(10, setScoreList).then((response) => {
+    firebase.getTopScore(limit, setScoreList).then((response) => {
       setIsLoading(false);
     });
   }, []);
@@ -16,13 +18,30 @@ const ScoreBoard = () => {
       {isLoading ? (
         <p> loading </p>
       ) : (
-        <div>
-          {scoreList.map((info, index) => (
-            <p key={info.username + index}>
-              {info.username}score {info.score}
-            </p>
-          ))}
-        </div>
+        <Table
+          bordered
+          striped
+          size="sm"
+          variant="dark"
+          className={styles.customTable}
+        >
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Username</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scoreList.map((info, index) => (
+              <tr key={info.username + index}>
+                <td>{index + 1}</td>
+                <td>{info.username}</td>
+                <td>{info.score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       )}
     </div>
   );
